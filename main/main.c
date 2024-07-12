0141ce396e0056e2b57e247d74e9bf46a63a5e34
TJL <1943993359@qwq.com>
2024-07-12 09:45:36 +0800

    mod main 2

--- main/main.c
+++ main/main.c
@@ -45,12 +45,11 @@ void app_main(void)
     // lv_demo_benchmark();
     ui_init();
     ui_init();
-    ui_init();
-    ui_init();
-    ui_init();
-    ui_init();
-    ui_init();
-    ui_init();
+    lvgl_driver_init(); // 初始化液晶驱动
+    lvgl_driver_init(); // 初始化液晶驱动
+    lvgl_driver_init(); // 初始化液晶驱动
+
+    lvgl_driver_init(); // 初始化液晶驱动
     ui_init();
     ui_init();
     ui_init();
